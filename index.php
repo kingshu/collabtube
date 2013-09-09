2d7e7369d12e9935d0e9bb4a371a7bdce3697a98
Krishnabh Medhi <kingshu.crusader@gmail.com>
2013-09-08 22:20:04 -0400

    cosmetic changes

--- index.php
+++ index.php
@@ -2,23 +2,47 @@
 <head>
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js"></script> 
 	<link rel="stylesheet" type="text/css" href="css/global.css">
+	<style>
+		.container {width:70%;}
+		.roomID {width:150px;}
+		.floatleft {float:left;}
+		.floatright {float:right;}
+		.center {text-align:center;}
+	</style>
 </head>
 
 <body>
-	<a href='localhost/collabtube'><h1>COLLABTUBE</h1></a>	
+	<div class="navbar navbar-default">
+	<h1>&emsp;&emsp;<a href='localhost/collabtube'>COLLABTUBE</a></h1> <br>
+	</div>
 	
-	<h3>Create New Room</h3>
-	<form action="create.php" method="POST" enctype="multipart/form-data"> 
-		<input id='createButton' type="submit" value="Create Room"/>	
-	</form>
-	
-	<h2>OR</h2><br>
-	
-	<h3>Join Existing Room</h3> 
-	<form action="join.php" method="POST" enctype="multipart/form-data"> 
-		<input type="text" name="roomID" placeholder="Room ID" />	
-		<input id='joinButton' type="submit" value="Join"/>	
-	</form>
-
+	<table class='centered container' border=0 cellpadding=25>
+		
+		<tr><td>
+			
+			<h3  class='floatright'>Create New Room</h3>
+			<form action="create.php" method="POST" enctype="multipart/form-data"> 
+				<input id='createButton' type="submit" value="Create Room" class="btn btn-primary btn-lg floatright"/>	
+			</form>
+		
+		</td><td class='center'>	
+		
+			<h2>|<br><br>OR<br><br>|</h2><br>
+		
+		</td><td>	
+		
+			<h3 class='floatleft'>Join Existing Room</h3> 
+			<form action="join.php" method="POST" enctype="multipart/form-data"> 
+			<table border=0 cellpadding=5 class='floatleft'>
+				<tr><td>
+					<input type="text" name="roomID" placeholder="Room ID" class="form-control input-lg roomID"/>	
+				</td><td>	
+					<input id='joinButton' type="submit" value="Join" class="btn btn-primary btn-lg"/>
+				</td></tr>	
+			</table>
+			</form>
+		
+		</td></tr>
+	</table>
 </body>	
 </html>
--- join.php
+++ join.php
@@ -1,4 +1,3 @@
-<link rel="stylesheet" type="text/css" href="css/global.css">
 <?php
 	$db = new PDO('mysql:dbname=collabtube;host=localhost','root','k9is1337!');
 	
@@ -8,22 +7,63 @@
     $result = $statement->fetchAll();
      
     if ( empty($result) ) {
-		echo "<a href='localhost/collabtube'><h1>COLLABTUBE</h1></a><br>";
-		
-		echo "<h2>This room does not exist! You can: </h2>";
-		
-			echo "<h3>Create New Room</h3>";
-			echo "<form action='create.php' method='POST' enctype='multipart/form-data'> ";
-				echo "<input id='createButton' type='submit' value='Create Room'/>	";
-			echo "</form>";
+		echo "<style>
+					.container {width:70%;}
+					.roomID {width:150px;}
+					.floatleft {float:left;}
+					.floatright {float:right;}
+					.center {text-align:center;}
+				</style>
 				
-			echo "	<h2>OR</h2><br>";
+				<script src='http://ajax.googleapis.com/ajax/libs/jquery/1.7/jquery.js'></script> 
+				<link rel='stylesheet' type='text/css' href='css/global.css'>				
+				<script>
+					$(document).ready(function() {
+						$('.close').click(function() {
+							$('#failMessage').fadeOut('slow');
+						});
+					});
+				</script>
 				
-			echo "	<h3>Try to another existing Room</h3> ";
-			echo "	<form action='join.php' method='POST' enctype='multipart/form-data'> ";
-			echo "		<input type='text' name='roomID' placeholder='Room ID' />	";
-		    echo "		<input id='joinButton' type='submit' value='Join'/>	";
-			echo "	</form>";
+				<div class='navbar navbar-default'>
+				<h1>&emsp;&emsp;<a href='localhost/collabtube'>COLLABTUBE</a></h1> <br>
+				</div>
+				
+				<table class='centered container' border=0 cellpadding=25>
+					
+					<tr><td colspan=3>
+						<div class='alert alert-dismissable alert-warning' id='failMessage'>
+							<button type='button' class='close' data-dismiss='alert'>&times;</button>
+							<h4><br>&emsp;&emsp;Room ".$_REQUEST['roomID']." does not exist.<br><br></h4>
+						</div>
+					</td></tr>
+					
+					<tr><td>
+						
+						<h3  class='floatright'>Create New Room</h3>
+						<form action='create.php' method='POST' enctype='multipart/form-data'> 
+							<input id='createButton' type='submit' value='Create Room' class='btn btn-primary btn-lg floatright'/>	
+						</form>
+					
+					</td><td class='center'>	
+					
+						<h2>|<br><br>OR<br><br>|</h2><br>
+					
+					</td><td>	
+					
+						<h3 class='floatleft'>Join Existing Room</h3> 
+						<form action='join.php' method='POST' enctype='multipart/form-data'> 
+						<table border=0 cellpadding=5 class='floatleft'>
+							<tr><td>
+								<input type='text' name='roomID' placeholder='Room ID' class='form-control input-lg roomID'/>	
+							</td><td>	
+								<input id='joinButton' type='submit' value='Join' class='btn btn-primary btn-lg'/>
+							</td></tr>	
+						</table>
+						</form>
+					
+					</td></tr>
+				</table>";
 	}
 	else
 		header ('Location: room.php?id='.$result[0][0]);
