d102eac3bd831c2791efd6674f8c8d7f87cce23b
kingshu <kmedhi@purdue.edu>
2015-04-09 15:03:08 -0400

    updated extension

--- CollabTube_ChromeExtension/bg.js
+++ CollabTube_ChromeExtension/bg.js
@@ -1,18 +1,22 @@
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
-	chrome.tabs.query({url:"http://ct-kingshu2.rhcloud.com/room*"}, function(tabs) {
-		if (tabs.length==0)
+  	chrome.tabs.query({url:"http://ct-kingshu2.rhcloud.com/room*"}, function(tabs) {
+		if (tabs.length==0) {
 			alert ("You aren't in any CollabTube room at the moment");
-		for (var i=0; i<tabs.length; i++)	
-			chrome.tabs.sendMessage(tabs[i].id, { ytURL:sender.url }, function(response) {
-				var opt = {
-				  type: "basic",
-				  title: "CollabTube Information",
-				  message: response.alert,
-				  iconUrl: "notif_icon.png"
-				};
-				chrome.notifications.create(response.uid, opt, function(c){});
-			});
+		}
+		else {
+			for (var i=0; i<tabs.length; i++) {
+				chrome.tabs.sendMessage(tabs[i].id, { ytURL:sender.url, ytTitle:request.video_title }, function(response) {
+					var opt = {
+						type: "basic",
+						title: "Added to CollabTube!",
+						message: response.alert,
+						iconUrl: "notif_icon.png"
+					};
+					chrome.notifications.create(response.uid, opt, function(c){});
+				});
+			}
+		}
 	}); 
   }
 );
--- CollabTube_ChromeExtension/mainScript.js
+++ CollabTube_ChromeExtension/mainScript.js
@@ -1,21 +1,29 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-  	var postinp = $("#postinput");
-  	if ( postinp.val().slice(-1) != " " )
-    	postinp.val( postinp.val()+" " );
-    postinp.val( request.ytURL.substring(23) + " " );
-    roomNum = $('#roomID').val();
-    sendResponse({
-    				uid: roomNum+request.ytURL.substring(31), 
-    			  	alert: "The video has been added to your CollabTube post in Room "+roomNum
-    			});
-  });
+// Differentiate between collabtube tabs and youtube tabs because content scripts matches both
 
-if(document.URL.indexOf("www.youtube.com/watch?v") != -1) {
+if(document.URL.indexOf("ct-kingshu2.rhcloud.com/room") != -1) {
+  chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+    	var postinp = $("#postinput");
+
+      // Add a space if there is no space after a non-zero length post
+      if ( postinp.val().length != 0 && postinp.val().slice(-1) != " " ) {
+      	postinp.val( postinp.val()+" " );
+      }
+
+      postinp.val( postinp.val() + request.ytURL.substring(23) + " " );
+      roomNum = $('#roomID').val();
+      sendResponse({
+  			uid: roomNum+request.ytURL.substring(31), 
+  			  alert: "The video titled \""+request.ytTitle+"\" has been added to your CollabTube post in Room "+roomNum
+  			});
+    }
+  );
+}
+
+if(document.URL.indexOf("www.youtube.com/watch?v") != -1) { 
 	$('#watch-headline-title').append("<button id='addToCT' style='float:right' class='yt-uix-button-default yt-uix-button'>Add to CollabTube</button>");
 	$('#addToCT').click( function() {
-		chrome.runtime.sendMessage( {addButton:"clicked"}, function(response) {
-		  console.log(response);
-		});
-	})
+		chrome.runtime.sendMessage( {video_title: $('#eow-title').attr('title') });
+    this.setAttribute("disabled", true);
+	});
 }
